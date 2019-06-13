fb54c4472690369337d965dfbe5597a2dc6a042c
Alpe6825 <51789043+Alpe6825@users.noreply.github.com>
2019-06-13 15:16:34 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+// (1) Include Header
+#include <opencv2/opencv.hpp>
+#include <opencv2/rgbd.hpp>
+
+int main(int argc, char** argv)
+{
+	// (2) Set Optimized
+	cv::setUseOptimized(true);
+
+	// (3) Open Video Capture
+	cv::VideoCapture capture(cv::VideoCaptureAPIs::CAP_INTELPERC);
+	if (!capture.isOpened()) {
+		return -1;
+	}
+
+	// (4) Retrieve Camera Parameters
+	const uint32_t width = static_cast<uint32_t>(capture.get(cv::CAP_INTELPERC_DEPTH_GENERATOR + cv::VideoCaptureProperties::CAP_PROP_FRAME_WIDTH));
+	const uint32_t height = static_cast<uint32_t>(capture.get(cv::CAP_INTELPERC_DEPTH_GENERATOR + cv::VideoCaptureProperties::CAP_PROP_FRAME_HEIGHT));
+	const float fx = static_cast<float>(capture.get(cv::CAP_PROP_INTELPERC_DEPTH_FOCAL_LENGTH_HORZ));
+	const float fy = static_cast<float>(capture.get(cv::CAP_PROP_INTELPERC_DEPTH_FOCAL_LENGTH_VERT));
+	const float cx = width / 2.0f - 0.5f;
+	const float cy = height / 2.0f - 0.5f;
+	const cv::Matx33f camera_matrix = cv::Matx33f(fx, 0.0f, cx, 0.0f, fy, cy, 0.0f, 0.0f, 1.0f);
+
+	// (5) Initialize KinFu Parameters
+	cv::Ptr<cv::kinfu::Params> params;
+	params = cv::kinfu::Params::defaultParams(); // Default Parameters
+	//params = cv::kinfu::Params::coarseParams(); // Coarse Parameters
+
+	params->frameSize = cv::Size(width, height); // Frame Size
+	params->intr = camera_matrix;             // Camera Intrinsics
+	params->depthFactor = 1000.0f;                   // Depth Factor (1000/meter)
+
+	// (6) Create KinFu
+	cv::Ptr<cv::kinfu::KinFu> kinfu;
+	kinfu = cv::kinfu::KinFu::create(params);
+
+	while (true) {
+		// (7) Grab All Frames and Retrieve Depth Frame
+		capture.grab();
+
+		cv::UMat frame;
+		capture.retrieve(frame, cv::CAP_INTELPERC_DEPTH_MAP);
+		if (frame.empty()) {
+			continue;
+		}
+
+		// (8) Flip Image
+		cv::flip(frame, frame, 1);
+
+		// (9) Update Frame
+		if (!kinfu->update(frame)) {
+			std::cout << "reset" << std::endl;
+			kinfu->reset();
+			continue;
+		}
+
+		// (10) Rendering
+		cv::UMat render;
+		kinfu->render(render);
+
+		// (11) Show Image
+		cv::imshow("Kinect Fusion", render);
+		const int32_t key = cv::waitKey(1);
+		if (key == 'r') {
+			kinfu->reset();
+		}
+		if (key == 'q') {
+			break;
+		}
+	}
+
+	cv::destroyAllWindows();
+
+	std::cin.get();
+
+	return 0;
+}
